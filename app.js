fc62553fcbbe6e3fad2fd65a44169ac3a82def31
cavesa10 <cavesa10@gmail.com>
2024-03-01 23:20:19 -0500

    puse a funcionar el boton de encriptar y el bonton de borrar seleccion

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-function autoGrow(textarea) {
-  textarea.style.height = 'auto';
-  textarea.style.height = (textarea.scrollHeight) + 'px';
+const containerSuccess = document.querySelector(".container_output_success")
+const containerFail = document.querySelector(".container_output_fail")
+const textoArea = document.querySelector('#text')
+const parrafo = document.querySelector('#parrafoSuccess')
+
+
+
+// textoArea.addEventListener("input", function() {
+//   containerFail.style.display = 'none'
+//   containerSuccess.style.display = 'inline-block'
+//   setTimeout(function() {
+//     parrafo.textContent = textoArea.value;
+//     parrafo.innerHTML = textoArea.value.replace(/\n/g, "<br>");
+//   }, 1000); // 1000 milisegundos = 1 segundo
+// });
+
+textoArea.addEventListener("input", function() {
+  var text = textoArea.value;
+  var newText = text.replace(/[^a-z\s\n]/g, "");
+  if (newText !== text) {
+    textoArea.value = newText;
+  }
+});
+
+function borrar() {
+  textoArea.value = ''
 }
+
+ function encriptar() {
+   containerFail.style.display = 'none'
+   containerSuccess.style.display = 'inline-block'
+   parrafo.innerHTML = textoArea.value.replace(/\n/g, "<br>");
+ }
