97345d37b3432c3a504df61f45e8db5322e49897
cavesa10 <cavesa10@gmail.com>
2024-03-04 22:06:17 -0500

    realiza la funcionalidad de desencriptar

--- app.js
+++ app.js
@@ -3,15 +3,6 @@ const containerFail = document.querySelector(".container_output_fail");
 const textoArea = document.querySelector("#text");
 const parrafo = document.querySelector("#parrafoSuccess");
 
-// textoArea.addEventListener("input", function() {
-//   containerFail.style.display = 'none'
-//   containerSuccess.style.display = 'inline-block'
-//   setTimeout(function() {
-//     parrafo.textContent = textoArea.value;
-//     parrafo.innerHTML = textoArea.value.replace(/\n/g, "<br>");
-//   }, 1000); // 1000 milisegundos = 1 segundo
-// });
-
 textoArea.addEventListener("input", function () {
   let text = textoArea.value;
   let newText = text.replace(/[^a-z\s\n]/g, "");
@@ -22,6 +13,7 @@ textoArea.addEventListener("input", function () {
 
 function borrar() {
   textoArea.value = "";
+  textoArea.focus();
 }
 
 function encriptar() {
@@ -30,19 +22,19 @@ function encriptar() {
 
   let cadenaEncriptar = textoArea.value;
 
-  let resultado = '';
+  let resultado = "";
   for (let i = 0; i < cadenaEncriptar.length; i++) {
     let char = cadenaEncriptar[i];
-    if (char === 'e') {
-      resultado = resultado + 'enter';
-    } else if (char === 'i') {
-      resultado = resultado + 'imes';
-    } else if (char === 'a') {
-      resultado = resultado + 'ai';
-    } else if (char === 'o') {
-      resultado = resultado + 'ober';
-    } else if (char === 'u') {
-      resultado = resultado + 'ufat';
+    if (char === "e") {
+      resultado +=  "enter";
+    } else if (char === "i") {
+      resultado +=  "imes";
+    } else if (char === "a") {
+      resultado +=  "ai";
+    } else if (char === "o") {
+      resultado +=  "ober";
+    } else if (char === "u") {
+      resultado +=  "ufat";
     } else {
       resultado = resultado + char;
     }
@@ -53,12 +45,75 @@ function encriptar() {
 function desencriptar() {
   containerFail.style.display = "none";
   containerSuccess.style.display = "inline-block";
-/*   let cadenaDesencriptar = textoArea.value;
-  cadenaDesencriptar = cadenaDesencriptar.replaceAll("ai", "a");
-  cadenaDesencriptar = cadenaDesencriptar.replaceAll("enter", "e");
-  cadenaDesencriptar = cadenaDesencriptar.replaceAll("imes", "i");
-  cadenaDesencriptar = cadenaDesencriptar.replaceAll("ober", "o");
-  cadenaDesencriptar = cadenaDesencriptar.replaceAll("ufat", "u");
-  parrafo.innerHTML = cadenaDesencriptar.replace(/\n/g, "<br>"); */
+  let cadenaDesencriptar = textoArea.value;
+
+  let resultado = "";
+  for (let i = 0; i < cadenaDesencriptar.length; i++) {
+    let char = cadenaDesencriptar[i];
+    if (char === "e") {
+      let nter =
+        cadenaDesencriptar[i + 1] +
+        cadenaDesencriptar[i + 2] +
+        cadenaDesencriptar[i + 3] +
+        cadenaDesencriptar[i + 4];
+      console.log(nter);
+      if (nter === "nter") {
+        resultado += char;
+        i += 4;
+      }
+    } else if (char === "i") {
+      let mes =
+        cadenaDesencriptar[i + 1] +
+        cadenaDesencriptar[i + 2] +
+        cadenaDesencriptar[i + 3];
+      if (mes === "mes") {
+        resultado += char;
+        i += 3;
+      }
+    } else if (char === "a") {
+      let ai = cadenaDesencriptar[i + 1];
+      if (ai === "i") {
+        resultado += char;
+        i++;
+      }
+    } else if (char === "o") {
+      let ber =
+        cadenaDesencriptar[i + 1] +
+        cadenaDesencriptar[i + 2] +
+        cadenaDesencriptar[i + 3];
+      if (ber === "ber") {
+        resultado += char;
+        i += 3;
+      }
+    } else if (char === "u") {
+      let fat =
+        cadenaDesencriptar[i + 1] +
+        cadenaDesencriptar[i + 2] +
+        cadenaDesencriptar[i + 3];
+      if (fat === "fat") {
+        resultado += char;
+        i += 3;
+      }
+    } else {
+      resultado += char;
+    }
+  }
+  console.log(resultado);
+  parrafo.innerHTML = resultado.replace(/\n/g, "<br>");
+
 }
+
+
+  function copiarAlPortapapeles() {
+    let texto = parrafo.innerText;
+    navigator.clipboard.writeText(texto)
+      .then(() => {
+        alert("El texto ha sido copiado al portapapeles.");
+        textoArea.focus();
+      })
+      .catch(err => {
+        console.error('Error al copiar al portapapeles: ', err);
+      });
+  }
+
 
